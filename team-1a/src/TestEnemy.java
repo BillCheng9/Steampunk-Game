c51f9adf3a1828137f446697fff440050e9dfec7
Michael Yang <myang@vassar.edu>
2023-02-20 19:54:12 +0000

    Delete TestEnemy.java

--- team-1a/src/TestEnemy.java
+++
@@ -1,7 +0,0 @@
-public class TestEnemy implements Enemy{
-    public String short_attack(){return "";};
-
-    public String charge_attack(){return "";};
-
-    public String increase_stat(){return "";};
-}
