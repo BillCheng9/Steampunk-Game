12158fc94fb204418d52e2b4b00ba98eb05f793d
Bill Cheng <bcheng@mail.cs.vassar.edu>
2023-02-20 14:48:56 -0500

    basic class structure

---
+++ TestEnemy.java
@@ -0,0 +1,7 @@
+public class TestEnemy implements Enemy{
+    public String short_attack(){return "";};
+
+    public String charge_attack(){return "";};
+
+    public String increase_stat(){return "";};
+}
---
+++ team-1a/src/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+    private int experience, gears;
+    private Stats playerStats;
+    public Player(){
+        //Placeholder values for first iteration
+        playerStats = new Stats (5, 5, 5);
+        experience = 5;
+        gears = 5;
+    }
+}
