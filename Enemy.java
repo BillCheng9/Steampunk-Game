b01afca14e03eb45af935d5fb9f57a86628f6344
Michael Yang <myang@mail.cs.vassar.edu>
2023-02-27 14:11:46 -0500

    enemy stuff

--- Enemy.java
+++ Enemy.java
@@ -1,5 +1,10 @@
+
 public interface Enemy {
 
+    String name = "enemy.TYPE";
+    int defense = -1;
+    int health = -1;
+    int damage = -1;
     public int short_attack() throws InterruptedException;
 
     public int charge_attack() throws InterruptedException;
