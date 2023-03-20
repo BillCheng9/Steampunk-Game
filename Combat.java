e8be228e4283f302b043a1f98bfee1b832331ba1
Michael Yang <myang@mail.cs.vassar.edu>
2023-03-20 14:49:12 -0400

    Finished Combat Controller!

--- Combat.java
+++ Combat.java
@@ -8,10 +8,10 @@ public class Combat{
 
     public String getInput() throws InterruptedException {
         Combat_Dialogue inputD = new Combat_Dialogue();
-        inputD.print("prompt");
+        inputD.displayPrompt();
         String action = inputD.grabInput();
         while ( (!action.equals("LIGHT")) && (!action.equals("HEAVY")) && (!action.equals("FLEE")) && (!action.equals("PET")) && (!action.equals("INVENTORY")) ){
-            inputD.print("invalid");
+            inputD.displayInvalid();
             action = inputD.grabInput();
         }
         return action;
@@ -20,7 +20,7 @@ public class Combat{
     public int combatTurn(Player p, Enemy e) throws InterruptedException {
         Combat_Dialogue inputD = new Combat_Dialogue(e);
         Combat_Dialogue inputD2 = new Combat_Dialogue();
-        inputD.print("start");
+        inputD.displayStart();
         Input executeAction;
         int retval = -1;
         int val;
@@ -30,7 +30,7 @@ public class Combat{
         while ( (p.healthCheck()) && (e.healthCheck()) ) {
             // show health
             inputD.pHealth(p.health);
-            inputD.eHealth(e.getName(), e.getHealth());
+            inputD.eHealth(e.getHealth());
 
             // player turn
             String input = getInput();
@@ -56,16 +56,16 @@ public class Combat{
                     val = p.attack1();
                     if (val > 0) {
                         int res = e.attacked(val);
-                        Combat_Dialogue dmg = new Combat_Dialogue(p, e, res);
-                        dmg.print("damage");
+                        Combat_Dialogue dmg = new Combat_Dialogue(res);
+                        dmg.displayDamage();
                     }
                     break;
                 case HEAVY:
                     val = p.attack2();
                     if (val > 0) {
                         int res = e.attacked(val);
-                        Combat_Dialogue dmg = new Combat_Dialogue(p, e, res);
-                        dmg.print("damage");
+                        Combat_Dialogue dmg = new Combat_Dialogue(res);
+                        dmg.displayDamage();
                     }
                     break;
                 case PET:
@@ -77,12 +77,12 @@ public class Combat{
                 case FLEE:
                     fled = p.flee();
                     if (fled) {
-                        inputD2.print("flee_t");
+                        inputD2.displayFlee_T();
                         retval = 2;
                         break;
                     }
                     else {
-                        inputD2.print("flee_f");
+                        inputD2.displayFlee_F();
                     }
                     break;
             }
@@ -97,7 +97,7 @@ public class Combat{
             }
 
             inputD.pHealth(p.health);
-            inputD.eHealth(e.getName(), e.getHealth());
+            inputD.eHealth(e.getHealth());
 
             // enemy turn
             int eVal = (int) (Math.random() * 10);
@@ -105,15 +105,15 @@ public class Combat{
                 val = e.short_attack();
                 if (val > 0) {
                     int res = p.attacked(val);
-                    Combat_Dialogue dmg = new Combat_Dialogue(p, e, res);
-                    dmg.print("enemy");
+                    Combat_Dialogue dmg = new Combat_Dialogue(res);
+                    dmg.displayEnemy();
                 }
             } else if (eVal <= 74) {
                 val = e.charge_attack();
                 if (val > 0) {
                     int res = p.attacked(val);
-                    Combat_Dialogue dmg = new Combat_Dialogue(p, e, res);
-                    dmg.print("enemy");
+                    Combat_Dialogue dmg = new Combat_Dialogue(res);
+                    dmg.displayEnemy();
                 }
             } else {
                 e.increase_stat();
@@ -164,12 +164,12 @@ public class Combat{
         if (val == -1) {
             val = combat.combatTurn(p, e);
             if (val == 0) {
-                textD.print("lose");
+                textD.displayLose();
                 exit(0);
             } else if (val == 1) {
-                textD.print("win");
+                textD.displayWin();
             } else if (val == 2) {
-                textD.print("finish");
+                textD.displayFinish();
             }
         }
 
