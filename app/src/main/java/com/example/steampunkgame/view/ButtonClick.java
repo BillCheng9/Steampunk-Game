95ed3421dcc1b606d1af15d2b8962e7fc31c9767
myang <myang@vassar.edu>
2023-04-10 13:24:21 -0400

    bug testing with circular

--- app/src/main/java/com/example/steampunkgame/view/ButtonClick.java
+++ app/src/main/java/com/example/steampunkgame/view/ButtonClick.java
@@ -1,24 +1,58 @@
 package com.example.steampunkgame.view;
 import android.content.Context;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
-import com.example.steampunkgame.databinding.ActivityMainBinding;
 
-public class ButtonClick implements IButtonClick, View.OnClickListener {
+import androidx.annotation.NonNull;
+
+import com.example.steampunkgame.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+public class ButtonClick implements IButtonClick {
 
     ActivityMainBinding binding;
+    Listener listener;
 
-    public ButtonClick(Context context) {
+    public ButtonClick(Context context, @NonNull Listener listener) {
         this.binding = ActivityMainBinding.inflate(LayoutInflater.from(context));
-        this.binding.lightAttackBTN.setOnClickListener(this);
+        this.listener = listener;
+        this.binding.lightAttackBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonClick.this.binding.dialogueText.setText("LIGHT");
+                listener.onBTNClick("LIGHT");
+            }
+        });
+        this.binding.heavyAttackBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonClick.this.binding.dialogueText.setText("HEAVY");
+            }
+        });
+        this.binding.fleeBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonClick.this.binding.dialogueText.setText("FLEE");
+            }
+        });
+        this.binding.invBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonClick.this.binding.dialogueText.setText("INVENTORY");
+            }
+        });
+        this.binding.petBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonClick.this.binding.dialogueText.setText("PET");
+            }
+        });
     }
 
     public View getRootView() {
         return binding.getRoot();
     }
 
-    @Override
-    public void onClick(View view) {
-        this.binding.dialogueText.setText("TEST");
-    }
+
 }
