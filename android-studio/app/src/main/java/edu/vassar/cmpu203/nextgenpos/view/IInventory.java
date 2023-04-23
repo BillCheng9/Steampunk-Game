f2f80a185748d1bd4a34958bf8ab22df1287b94b
bcheng <bcheng@vassar.edu>
2023-04-23 18:59:05 -0400

    Added inventory screen prototype and related interface and classes

---
+++ android-studio/app/src/main/java/edu/vassar/cmpu203/nextgenpos/view/IInventory.java
@@ -0,0 +1,11 @@
+package edu.vassar.cmpu203.nextgenpos.view;
+
+public interface IInventory {
+    interface Listener{
+
+        /**
+         * Brings up item 1 details
+         */
+        void item1Click();
+    }
+}
---
+++ android-studio/app/src/main/java/edu/vassar/cmpu203/nextgenpos/view/Inventory.java
@@ -0,0 +1,30 @@
+package edu.vassar.cmpu203.nextgenpos.view;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import edu.vassar.cmpu203.nextgenpos.databinding.InventoryBinding;
+
+public class Inventory implements IInventory {
+
+    InventoryBinding binding;
+    Listener listener;
+
+    public Inventory(Context context, Listener listener){
+        this.listener = listener;
+        this.binding = InventoryBinding.inflate(LayoutInflater.from(context));
+
+        this.binding.sword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.item1Click();
+            }
+        });
+    }
+
+    public void buttonClickable(boolean clickable) {
+        Inventory.this.binding.sword.setClickable(clickable);
+    }
+
+}
