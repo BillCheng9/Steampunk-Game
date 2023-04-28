fd6a2dca83fabbfeb96e1d1e69c7db05a7e6412a
bcheng <bcheng@vassar.edu>
2023-04-28 17:15:17 -0400

    Added workshop xml

--- android-studio/app/src/main/java/edu/vassar/cmpu203/nextgenpos/view/Workshop.java
+++ android-studio/app/src/main/java/edu/vassar/cmpu203/nextgenpos/view/Workshop.java
@@ -1,5 +1,10 @@
 package edu.vassar.cmpu203.nextgenpos.view;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import edu.vassar.cmpu203.nextgenpos.databinding.InventoryBinding;
 import edu.vassar.cmpu203.nextgenpos.databinding.WorkshopBinding;
 import edu.vassar.cmpu203.nextgenpos.model.Player;
 
@@ -10,4 +15,79 @@ public class Workshop implements IWorkshop{
     Listener listener;
 
     Player p;
+
+    public Workshop(Context context, Listener listener, Player p){
+        this.listener = listener;
+        this.binding = WorkshopBinding.inflate(LayoutInflater.from(context));
+
+        this.binding.goBackBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.backClick();
+            }
+        });
+
+        this.binding.steelPlates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.plates1Click();
+            }
+        });
+
+        this.binding.tungstenPlates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.plates2Click();
+            }
+        });
+
+        this.binding.chromiumPlates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.plates3Click();
+            }
+        });
+
+        this.binding.xt1Nanites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.nanites1Click();
+            }
+        });
+
+        this.binding.xt3Nanites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.nanites2Click();
+            }
+        });
+
+        this.binding.xtpNanites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.nanites3Click();
+            }
+        });
+
+        this.binding.mechaGauntlets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.gauntlets1Click();
+            }
+        });
+
+        this.binding.cbaImplants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.implants1CLick();
+            }
+        });
+
+        this.binding.ice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.illegal1Click();
+            }
+        });
+    }
 }
