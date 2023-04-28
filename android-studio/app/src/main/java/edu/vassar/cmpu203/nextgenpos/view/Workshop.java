f0c43c148d4efe586fa0be5d38a059c0ef4ea5db
bcheng <bcheng@vassar.edu>
2023-04-28 17:08:42 -0400

    Added workshop xml

---
+++ android-studio/app/src/main/java/edu/vassar/cmpu203/nextgenpos/view/Workshop.java
@@ -0,0 +1,13 @@
+package edu.vassar.cmpu203.nextgenpos.view;
+
+import edu.vassar.cmpu203.nextgenpos.databinding.WorkshopBinding;
+import edu.vassar.cmpu203.nextgenpos.model.Player;
+
+public class Workshop implements IWorkshop{
+
+    WorkshopBinding binding;
+
+    Listener listener;
+
+    Player p;
+}
